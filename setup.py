a8151bc56adc9fb18c0fc2c87bf3d3aa4ba6cac5
Hiroyuki Tanaka <aflc0x@gmail.com>
2011-12-30 14:27:21 +0900

    first commit of core files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pyter',
+      version='0.1',
+      description='Simple ffuzzy matching library using Translation Error Rate algorithm',
+      author='Hiroyuki Tanaka',
+      author_email='aflc0x@gmail.com',
+      url='https://github.com/aflc/pyter',
+      packages=['pyter'],
+      install_requires=['distribute'],
+      )
