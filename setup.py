be4ecb23d645ec9a2fda03dbcc485b19e261d79b
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-01-13 23:47:40 +0900

    Add argparse requirement in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+import sys
+
+install_requires = ['distribute']
+if sys.hexversion < 0x2070000:
+    install_requires += ['argparse']
 setup(name='pyter',
       version='0.1',
       description='Simple ffuzzy matching library using Translation Error Rate algorithm',
@@ -6,5 +11,5 @@ setup(name='pyter',
       author_email='aflc0x@gmail.com',
       url='https://github.com/aflc/pyter',
       packages=['pyter'],
-      install_requires=['distribute'],
+      install_requires=install_requires,
       )
