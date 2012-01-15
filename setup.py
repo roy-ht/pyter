21799b1c464c0c0baba7e7dc26fb5c3a9f791d1a
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-01-15 20:04:50 +0900

    Add long description via README in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 import sys
+import os
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 install_requires = ['distribute']
 if sys.hexversion < 0x2070000:
@@ -7,6 +15,7 @@ if sys.hexversion < 0x2070000:
 setup(name='pyter',
       version='0.1.1',
       description='Simple ffuzzy matching library using Translation Error Rate algorithm',
+      long_description=read('README.rst'),
       author='Hiroyuki Tanaka',
       author_email='aflc0x@gmail.com',
       url='https://github.com/aflc/pyter',
