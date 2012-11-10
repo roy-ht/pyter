a9bdc619cdccc548739384b38940d44adb1783d9
Hiroyuki Tanaka <aflc0x@gmail.com>
2012-11-10 10:32:31 +0900

    Fixed: raw unicode literals for python3 and python2

--- pyter/util.py
+++ pyter/util.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from __future__ import unicode_literals
 
 import re
 import unicodedata
@@ -13,11 +14,11 @@ def preprocess_en(s):
     記号 <=> 文字以外
     """
     s = s.rstrip()  # trail space, tab, newlineの削除
-    s = re.sub(ur'([^a-zA-Z0-9])(?!\1|$)', ur'\1 ', s) # 前が記号で、後ろが出た文字以外
-    s = re.sub(ur'(?<=[a-zA-Z0-9])([^a-zA-Z0-9])', ur' \1', s) # 後ろが記号で、前が記号以外
-    s = re.sub(ur'\s+', ur' ', s)  # スペースの個数正規化
-    s = re.sub(ur'(\d) ([.,]) (\d)', ur'\1\2\3', s)  # 0 . 1 -> 0.1
-    s = re.sub(ur'(Dr|Jr|Prof|Rev|Gen|Mr|Mt|Mrs|Ms) .', ur'\1.', s)  # Mr . -> Mr.
+    s = re.sub(r'([^a-zA-Z0-9])(?!\1|$)', r'\1 ', s) # 前が記号で、後ろが出た文字以外
+    s = re.sub(r'(?<=[a-zA-Z0-9])([^a-zA-Z0-9])', r' \1', s) # 後ろが記号で、前が記号以外
+    s = re.sub(r'\s+', ur' ', s)  # スペースの個数正規化
+    s = re.sub(r'(\d) ([.,]) (\d)', ur'\1\2\3', s)  # 0 . 1 -> 0.1
+    s = re.sub(r'(Dr|Jr|Prof|Rev|Gen|Mr|Mt|Mrs|Ms) .', ur'\1.', s)  # Mr . -> Mr.
     s = s.replace(u'e . g .', u'e.g.')
     s = s.replace(u'i . e .', u'e.g.')
     s = s.replace(u'U . S .', u'U.S.')
