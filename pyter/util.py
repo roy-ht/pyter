4dd25453eed12ee2590ce35510a9b45b1d3d3b4d
Alexander Raginsky <alexander.raginsky@gmail.com>
2017-01-04 14:20:09 +0200

    Fixes for python 3

--- pyter/util.py
+++ pyter/util.py
@@ -16,9 +16,9 @@ def preprocess_en(s):
     s = s.rstrip()  # trail space, tab, newlineの削除
     s = re.sub(r'([^a-zA-Z0-9])(?!\1|$)', r'\1 ', s) # 前が記号で、後ろが出た文字以外
     s = re.sub(r'(?<=[a-zA-Z0-9])([^a-zA-Z0-9])', r' \1', s) # 後ろが記号で、前が記号以外
-    s = re.sub(r'\s+', ur' ', s)  # スペースの個数正規化
-    s = re.sub(r'(\d) ([.,]) (\d)', ur'\1\2\3', s)  # 0 . 1 -> 0.1
-    s = re.sub(r'(Dr|Jr|Prof|Rev|Gen|Mr|Mt|Mrs|Ms) .', ur'\1.', s)  # Mr . -> Mr.
+    s = re.sub(r'\s+', r' ', s)  # スペースの個数正規化
+    s = re.sub(r'(\d) ([.,]) (\d)', r'\1\2\3', s)  # 0 . 1 -> 0.1
+    s = re.sub(r'(Dr|Jr|Prof|Rev|Gen|Mr|Mt|Mrs|Ms) .', r'\1.', s)  # Mr . -> Mr.
     s = s.replace(u'e . g .', u'e.g.')
     s = s.replace(u'i . e .', u'e.g.')
     s = s.replace(u'U . S .', u'U.S.')
